43134429494e7070605a671ecc55b52ef41d9661
kimwonmyong <kimwonmyong@gmail.com>
2024-09-20 11:43:48 +0900

    .map 작업 완료

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,34 +1,44 @@
 import React from 'react';
 import { TiHome } from 'react-icons/ti';
-// import { TiHomeOutline } from 'react-icons/ti';
+import { TiHomeOutline } from 'react-icons/ti';
 import { FaPenToSquare } from 'react-icons/fa6';
-// import { FaRegPenToSquare } from 'react-icons/fa6';
+import { FaRegPenToSquare } from 'react-icons/fa6';
 import { IoPerson } from 'react-icons/io5';
-// import { IoPersonOutline } from 'react-icons/io5';
+import { IoPersonOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const navList = [
+    {
+      id: 1,
+      pathname: '/',
+      icon: <TiHome size={'24px'} />,
+      activeIcon: <TiHomeOutline size={'24px'} />,
+    },
+    {
+      id: 2,
+      pathname: '/post',
+      icon: <FaPenToSquare size={'24px'} />,
+      activeIcon: <FaRegPenToSquare size={'24px'} />,
+    },
+    {
+      id: 3,
+      pathname: '/profile',
+      icon: <IoPerson size={'24px'} />,
+      activeIcon: <IoPersonOutline size={'24px'} />,
+    },
+  ];
+
   return (
     <nav className="fixed bottom-0 bg-churead-gray-800 w-full max-w-[572px]">
       <ul className="flex justify-center">
-        <li>
-          <Link to="/" className="block p-6">
-            <TiHome size={'24px'} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="block p-6">
-            <FaPenToSquare size={'24px'} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="block p-6">
-            <IoPerson size={'24px'} />
-          </Link>
-        </li>
-        {/* <TiHomeOutline /> */}
-        {/* <FaRegPenToSquare /> */}
-        {/* <IoPersonOutline /> */}
+        {navList.map((nav) => (
+          <li key={nav.id}>
+            <Link to={nav.pathname} className="block p-6">
+              {nav.icon}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
