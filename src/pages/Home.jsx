16958acd2e7afb590e7faf3c4310b43fb867a24e
kimwonmyong <kimwonmyong@gmail.com>
2024-09-23 09:50:09 +0900

    과제 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import FeedItem from '../components/FeedItem';
 import Header from '../components/layout/Header';
 import Nav from '../components/layout/Nav';
 import { initiaFeedList } from '../data/response';
 
-const Home = () => {
+const Home = ({ churead }) => {
   const [feedList, setFeedList] = useState(initiaFeedList);
+
+  useEffect(() => {
+    if (!churead) return;
+    const newFeed = {
+      id: feedList.length + 1,
+      userName: 'anonymois',
+      userProfileImage:
+        'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y',
+      churead: churead,
+      likeCount: 38,
+    };
+    setFeedList([newFeed, ...feedList]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="h-full pt-20 pb-[74px] overflow-hidden">
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import PostInput from '../components/PostInput';
 
-const Post = () => {
+const Post = ({ onPost }) => {
+  const [churead, setChuread] = useState('');
+
+  const history = useNavigate();
+
+  const handlePost = (event) => {
+    event.preventDefault();
+    onPost(churead);
+    history('/');
+  };
+
+  const handleChange = (value) => {
+    setChuread(value);
+  };
+
   return (
     <div className="h-full">
       <header className="fixed max-w-[572px] mx-auto px-4 py-6 text-center top-0 left-0 right-0">
@@ -18,8 +32,8 @@ const Post = () => {
       </header>
       <main className="h-full pt-[72px] pb-[88px] overflow-hidden">
         <div className="h-full overflow-auto">
-          <form id="post">
-            <PostInput />
+          <form id="post" onSubmit={handlePost}>
+            <PostInput onChange={handleChange} />
             <div className="w-full max-w-[572px] flex items-center fixed bottom-0 lef p-6">
               <p className="text-churead-gray-300 text-opacity-60">
                 누구에게나 답글 및 인용 허용
